4faf94a806b99c323662d0c84a07d5a8923e4350
Vitaliy Rogov <vv55rogov@gmail.com>
2023-06-03 08:16:52 +0600

    add main

---
+++ src/test/java/ru/netology/services/ServceTest.java
@@ -0,0 +1,27 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+public class ServceTest {
+
+    @Test
+    public void testProgram1(){
+        Service service = new Service();
+        int expected = 3;
+        int actual = service.calculate(10000, 3000, 20000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testProgram2(){
+             Service service = new Service();
+             int expected = 2;
+             int actual = service.calculate(100000, 60000, 150000);
+
+            Assertions.assertEquals(expected, actual);
+    }
+}
